2668033924607c239ea34d77ff20140466ad7443
unknown <kalpesh@cnergee.com>
2025-11-18 19:53:05 +0530

    :q lease enter the commit message for your changes. Lines starting

--- server/userauth_srv/route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const auth_modal = require('./userauth/userauth_modal');
-
-router.post('/user-login', auth_modal.login_UserModal);
-
-module.exports = router;
